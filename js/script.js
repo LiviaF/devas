b8f9a1cd5ea7672bba76bcc317b7d0c6769f23e5
Livia Freitas <livia.freitas.94@gmail.com>
2021-04-29 22:50:42 -0300

    [WIP]get inputs and selects of form in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 function saveForm(){
-    const name = document.getElementById("nameInput").value
+    const form = document.querySelector('form')
 
-    const newDiv = document.createElement("div")
-    newDiv.innerHTML = name
-    document.body.appendChild(newDiv)
+    const data = form.querySelectorAll('input,select')
+    console.log(data)
+
+    for (let i=0; i < data.length; i++){
+
+        const data_item = data[i].value
+
+        //se item for uma tag select verificar se o selected esta true
+        console.log(data_item)
+
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = data_item
+        document.body.appendChild(newDiv)
+    }
 
 
 }
